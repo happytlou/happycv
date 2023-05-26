5a9090a88869d379d9f9e9eb76fafb5f50725544
Happy Tlou <happykamogelo33@gmail.com>
2023-05-26 10:25:20 +0200

    added all project

---
+++ src/content_option.js
@@ -0,0 +1,67 @@
+const logotext = "HAPPY";
+const meta = {
+    title: "Happy Legoale",
+    description: "I’m Happy Legoale data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "I’m Happy Legoale",
+    animated: {
+        first: "I love coding",
+        second: "I develop mobile apps",
+    },
+    description: "I code cool websites",
+    your_img_url: "https://lh3.googleusercontent.com/EvzYbwBeRbVrm4e0W1Zquhp8e_EjY7FchHi-A8MDOIRvc2q3b6GfMed9OmWpV_RmD-4GXYrcau8x5kfsc_TTRioLOYrr817oTyb9J42brnrbvz9LVYciVizgT1Zbme8bXXNkGtkL2COpX7JWdS--Vi1uUexRrP1nHyXMnOkm8UvBllOnXElwi1ew5Nu06zV1ar9usqePfPRFTjCS8OIxMV3hcDXKshH67AdsJLOUMyeFWiLKlMLPCpPVnEgR3r5_cgj_yfI8raiwlgG1DgM0gvrB_RWvBWLwbmp8goFUqie7OMWo2wjURpN4A1M0B9Nc6AX7h-fTRhpI8OQlBqoxyO1qb1wlqCUH2iQIOLiDVtUHMR69JmTfI8_efPMZVCKWIxzN2g44c2hat8fk5toC5To6e9owmQLc9s1xpznQbLUOV0HVd-6o04SBgvEctDKDWCaNblEfCooGkAzt1utZ2x7IUqeadAxaXdFbqKi8sQAktJV16EPpT9GiGQV_INkaWZ9uXBAQR0_AZXKlMm3voWYPZqMWoxuDAHnQ8nA64WAtapULIRdbrvytMDVs-AKoN3K8AhjHTCPRA2FZLLjhKjokfeas0QFFF-KDBEKkpg2LJRizZRBKm5mhqoAqNFWRZG9qog8qbOzVnJ-fUiixj3kqGlxHO8xnveOPmLHebRYTXrnZ34FDr_CuPr-TTwAkQAXaO2RUS9pxCdWCaDeTLO5WKMYZTmXTLkECZ-SWvYH0_6lw3StReCp9-q8xmgYoi2gil852d7c1sRYMtVXmsZ0TH82-HieRDEJQyEVbvX3lDAR77ui46zGt-_tdjzO447Ravi8DylHFlH7GSxLnSKtS9nF_1p9nBwi51NTOFElA87Wt2jzjK9-V68ljqEJhl6DTbu96TACf1w81HGEXszekCwM9D1Mqwn3FFwur_7YYCSni=w615-h821-s-no?authuser=0",
+};
+
+const dataabout = {
+    title: "about my self",
+    aboutme: "My name is Happy Legoale, I am 28 years old. I live in Tshepisong Soweto, I studied 5 year at NID College in Cape Town. I studied 3 year at Code College doing Coding/Programming. I am currently a infystart at the digital academy.",
+};
+
+
+const skills = [{
+        name: "HTML",
+        value: 100,
+    },
+    {
+        name: "CSS",
+        value: 65,
+    },
+    {
+        name: "Javascript",
+        value: 60,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "GitHub",
+        value: 85,
+    },
+];
+
+
+const contactConfig = {
+    YOUR_EMAIL: "happykamogelo33@gmail.com",
+    YOUR_FONE: "071 163 3125",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+};
+export {
+    meta,
+    dataabout,
+    skills,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
